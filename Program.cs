ed0e89e9c0a670de7b6d022e2566972aaf976d23
KaioRapanos <kaiorapanos@gmail.com>
2023-10-15 14:27:49 -0300

    Realizando Herança e Polimorfismo em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("************************");
+Console.WriteLine("Smartphone Nokia: ");
+
+Smartphone nokia = new Nokia("41-99876-5432","K500","123456789",128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("************************");
+Console.WriteLine("Smartphone Iphone: ");
+
+Smartphone iphone = new Iphone("41-99876-5432","Iphone15","123456789",256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
